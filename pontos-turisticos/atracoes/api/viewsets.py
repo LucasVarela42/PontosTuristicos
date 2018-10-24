f3cbe5f84a521d6ba3a801856d80e7e643cb5dcf
Lucas Varela <lucaas.varela@gmail.com>
2018-10-24 17:55:02 -0200

    Adicionado autenticação e autorização para pontos turisticos

--- pontos-turisticos/atracoes/api/viewsets.py
+++ pontos-turisticos/atracoes/api/viewsets.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from atracoes.api.serializers import AtracaoSerializer
@@ -7,3 +8,6 @@ from atracoes.models import Atracao
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('nome', 'descricao')
+
--- pontos-turisticos/pontos_turisticos/urls.py
+++ pontos-turisticos/pontos_turisticos/urls.py
@@ -25,6 +25,8 @@ from enderecos.api.viewsets import EnderecoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 from avaliacoes.api.viewsets import AvaliacaoViewSet
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 router = routers.DefaultRouter()
 router.register(r'pontosturisticos', PontoTuristicoViewSet, base_name='PontoTuristico')
 router.register(r'atracoes', AtracaoViewSet)
@@ -35,4 +37,5 @@ router.register(r'avaliacoes', AvaliacaoViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
